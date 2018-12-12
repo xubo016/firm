c71efdb5b358df6f6aa7e1eebda3884cffce3cfa
xubo016 <41979134+xubo016@users.noreply.github.com>
2018-12-12 23:39:36 +0800

    无限极

--- application/admin/controller/Cate.php
+++ application/admin/controller/Cate.php
@@ -5,6 +5,13 @@ use app\admin\model\Cate as CateModel;
 class Cate extends Common
 {
   /**
+   * 前置操作
+   */
+  protected $beforeActionList = [
+    'delsoncate'  =>  ['only'=>'del'],
+  ];
+
+  /**
    * 栏目添加
    */
   public function add(){
@@ -18,7 +25,7 @@ class Cate extends Common
       }
       return;
     }
-    $code = $cate->select();
+    $code = $cate->catetree();
     $this->assign("value",$code);
     return view();
   }
@@ -27,8 +34,34 @@ class Cate extends Common
    * 栏目列表
    */
   public function lst(){
+    $cate = new CateModel;
+    $res = $cate->catetree();
+    $this->assign('cate',$res);
     return view();
   }
 
+  /**
+   * 栏目删除
+   */
+  public function del(){
+    $del = db('cate')->delete(input('id'));
+    if($del){
+      $this->success('删除成功');
+    }else{
+      $this->error('删除失败');
+    }
+  }
+
+  /**
+   * 无限极删除
+   */
+  public function delsoncate(){
+    $cateid = input('id'); //要删除的当前栏目id
+    $cate = new CateModel;
+    $son = $cate->getchilrenid($cateid);
+    if($son){
+      db('cate')->delete($son);
+    }
+  }
 }
 ?>
--- application/admin/model/Cate.php
+++ application/admin/model/Cate.php
@@ -17,9 +17,49 @@ class Cate extends Model
       return false;
     }
   }
+
+  /**
+   * 栏目查询
+   */
+  public function catetree(){
+    $cate = $this->select();
+    return $this->sort($cate);
+  }
+  //回调函数
+  public function sort($data,$pid=0,$level=0){
+    static $arr = array();
+    foreach($data as $k => $v){
+      if($v['pid']==$pid){
+        $v['level']=$level;
+        $arr[] = $v;
+        $this->sort($data,$v['id'],$level+1);
+      }
+    }
+    return $arr;
+  }
+
+  /**
+   * 无限极删除
+   */
+  public function getchilrenid($cateid){
+    $cateres = $this->select();
+    return $this->_getchilrenid($cateres,$cateid);
+  }
+
+  //删除的回调函数
+  public function _getchilrenid($cateres,$pid){
+    static $arr = array();
+    foreach($cateres as $k=>$v){
+      if($v['pid']==$pid){
+        $arr[]=$v['id'];
+        $this->_getchilrenid($cateres,$v['id']);
+      }
+    }
+    return $arr;
+  }
+
+
+
+
 }
-
-
-
-
 ?>
