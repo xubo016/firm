3320c231c4dac3ba190eaef155e31cfa82f1b187
xubo016 <41979134+xubo016@users.noreply.github.com>
2018-12-17 00:25:33 +0800

    配置项

--- application/admin/controller/Conf.php
+++ application/admin/controller/Conf.php
@@ -11,6 +11,9 @@ class Conf extends Common
     $conf = new ConfModel;
     if(request()->isPost()){
       $data = input('post.');
+      if($data['values']){
+        $data['values']=str_replace('，',',',$data['values']);
+      }
       if($conf->save($data)){
         $this->success('添加成功');
       }else{
@@ -25,6 +28,8 @@ class Conf extends Common
    * 配置列表
    */
   public function lst(){
+    $confres = ConfModel::paginate(10);
+    $this->assign('conf',$confres);
     return view();
   }
 
@@ -32,6 +37,48 @@ class Conf extends Common
    * 配置修改
    */
   public function edit(){
+    if(request()->isPost()){
+      $res = new ConfModel;
+      $data = input('post.');
+      if($data['values']){
+        $data['values'] = str_replace('，',',',$data['values']);
+      }
+      if($res->save($data,['id'=>input('id')])){
+        $this->success('修改成功','lst');
+      }else{
+        $this->error('修改失败');
+      }
+      return;
+    }
+    $conf = db('conf')->find(input('id/d'));
+    $this->assign('conf',$conf);
+    return view();
+  }
+
+  /** 
+   * 配置删除
+   */
+  public function del(){
+    $id = input('id/d');
+    if(db('conf')->delete($id)){
+      $data = [
+        'statu' => 200,
+        'info'  => '删除成功'
+      ];
+    }else{
+      $data = [
+        'statu'   => 400,
+        'info'  => '删除失败'
+      ];
+    }
+    return $data;
+  }
+  /** 
+   * 配置项
+   */
+  public function conf(){
+    $confres = db('conf')->select();
+    $this->assign('conf',$confres);
     return view();
   }
 
